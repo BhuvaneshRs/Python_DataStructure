6038c2773fdbc12c936ca5e62286b5e887efb30b
Bhuvaneshwara Ragavendra Srinivasan <132887722+BhuvaneshRs@users.noreply.github.com>
2023-06-09 22:19:47 +0530

    creating tuple

---
+++ creating_tuple.py
@@ -0,0 +1,27 @@
+# Tuple
+#Creating Tuple
+a=()
+a=tuple()
+
+#Its an iterable, Immutable, hetrogenity and Ordered.
+a=(1,2,3,4,5)
+a[0]
+1
+a[4]
+5
+a=("Bhuvanesh",24,"salem")
+a
+('Bhuvanesh', 24, 'Salem')
+a[:]
+('Bhuvanesh', 24, 'Salem')
+a[1:]
+(24, 'Salem')
+
+#count
+a.count(24)
+1
+
+#Index
+a=("Bhuvanesh",24,"Salem")
+a.index("Salem")
+2
